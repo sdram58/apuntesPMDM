9a9a565e0efaea9627b3bf79dcdc1147b78f68df
sdram58 <sdram58@hotmail.com>
2021-09-01 20:35:38 +0200

    seguimos

--- script/script.js
+++ script/script.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", start);
-
+/*
 function start(){
     navigator.clipboard.writeText("<empty clipboard>").then(function() {
-        /* clipboard successfully set */
-        alert("ok");
+        /* clipboard successfully set 
+        //alert("ok");
       }, function() {
-        /* clipboard write failed */
+        /* clipboard write failed 
         alert("ko");
       });
 }
+*/
