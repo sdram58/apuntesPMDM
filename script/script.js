318a52343d7e2552c4b55a3161c51876d3f53bb5
sdram58 <sdram58@hotmail.com>
2023-09-08 00:16:06 +0200

    Menú lateral, versión 1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+var posicion = 300;
 window.addEventListener("load", start);
 /*
 function start(){
@@ -10,3 +11,28 @@ function start(){
       });
 }
 */
+
+function start(){
+
+  const primerEnlace = document.querySelectorAll('body > a')[0];
+
+// Obtener la posición del elemento en el documento
+  posicion = primerEnlace.getBoundingClientRect().bottom;
+
+  window.addEventListener("scroll", mostrarElemento);
+}
+
+// Función para mostrar u ocultar el elemento al hacer scroll
+function mostrarElemento() {
+  const elemento = document.getElementById("menu-lateral");
+  const posicionScroll = window.scrollY;
+  //const umbral = 200; // Cambia este valor según tus preferencias
+  
+  if (posicionScroll > posicion) {
+    elemento.classList.remove("oculto");
+  } else {
+    elemento.classList.add("oculto");
+  }
+}
+
+// Agregar un evento de desplazamiento (scroll) al documento
